67db3e1ad7cf5def991e70089182deb19f35435d
Mayankjat05 <115417820+Mayankjat05@users.noreply.github.com>
2024-07-06 23:09:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,4 +10,4 @@ API_ID = int(os.environ.get("API_ID", "23159366"))
 API_HASH = os.environ.get("API_HASH", "4623dd30dd1303bddb729eb0862262d9")
 
 #Database 
-DB_URI = os.environ.get("DB_URI", "mongodb+srv://mayank:mayank@cluster0.dood4ni.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_URI = os.environ.get("DB_URI", "mongodb+srv://strangemods11:strangemods11@cluster0.vuftfsw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0") 
